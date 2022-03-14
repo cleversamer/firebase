6ff5031a5876d41c917bbecc4a558e9aeb8f78fa
Samer A <thedev.samer@gmail.com>
2022-03-14 11:16:01 +0200

    added README file

---
+++ README.md
@@ -0,0 +1,18 @@
+# Firebase V9
+
+## Installation
+
+- Write `npm inatall` on the terminal
+
+## Execution
+
+- Open a terminal window, and then write `npm run build`
+  - This will keep listening on changes, and then update the bundle.js file.
+  - After it updates the bundle.js file, you will be able to see the console.log results on browser.
+- Open another terminal window, and then write `npm start`
+  - This an optional package to install.
+  - It keeps waiting for any file change in the project, and then it reruns on the terminal.
+
+## Authors
+
+- Authored by [Samer A.](https://twitter.com/ssadawi__)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello");
+const { initializeApp } = require("firebase/app");
+const { getFirestore, collection, getDocs } = require("firebase/firestore");
+
+const config = {
+  apiKey: "AIzaSyCKYC5X_1erN17u-dAEPwsbdpzEpXePWgI",
+  authDomain: "fir-v9-8cf1a.firebaseapp.com",
+  projectId: "fir-v9-8cf1a",
+  storageBucket: "fir-v9-8cf1a.appspot.com",
+  messagingSenderId: "307851376705",
+  appId: "1:307851376705:web:524938cb79fedd12bcaec0",
+};
+
+// init firebase app
+initializeApp(config);
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const collectionRef = collection(db, "books");
+
+// get collection data
+getDocs(collectionRef).then((snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => {
+    books.push({ id: doc.id, ...doc.data() });
+  });
+  console.log(books);
+});
